63abbfd05e97fcfe326ac6e391c144676275aee9
dkessner laptop <Darren.Kessner@marlborough.org>
2022-04-06 11:45:55 -0700

    modified:   gc_content.sh 	modified:   hello.sif
    fixed order of operations

--- day4/gc_content.sh
+++ day4/gc_content.sh
@@ -12,6 +12,6 @@ C_count=$(echo $sequence | grep -o  C | wc -l)
 G_count=$(echo $sequence | grep -o  G | wc -l)
 T_count=$(echo $sequence | grep -o  T | wc -l)
 total=$(echo "$A_count + $C_count + $G_count + $T_count" | bc -l)
-gc_content=$(echo "$G_count + $C_count / $total" | bc -l)
+gc_content=$(echo "($G_count + $C_count) / $total" | bc -l)
 echo $gc_content
 
